78f6eb132b78a1391731a591ff69430aceafc20e
Rafael Batista <rafael.batista@iugu.com>
2022-04-26 09:47:22 -0300

    Iniciando configuração do devise_token_auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
+require 'api_constraints'
+
 Rails.application.routes.draw do
+  namespace :api, defaults: { format: :json } do
+    mount_devise_token_auth_for 'User', at: 'auth'
+
+    scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
+      # resources :articles
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
