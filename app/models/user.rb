8cd1a4a3977995b3c499fc60c4fcce22bae7d9b4
Rafael Batista <rafael.batista@iugu.com>
2022-04-26 16:43:32 -0300

    Acertando as configurações do devise_token_auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@
 
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :confirmable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable 
   include DeviseTokenAuth::Concerns::User
 end
